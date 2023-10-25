9c359f42a9b8b5f55a6522c3120c69ec76664110
tutor-yun <jslovejs182@gmail.com>
2023-10-25 11:39:29 +0900

    divide() 작성

--- src/Main.java
+++ src/Main.java
@@ -53,11 +53,11 @@ public class Main {
     }
 
     private static void multiply(int a, int b) {
-        System.out.println(a + " - " + b + " = " + (a - b));
+        System.out.println(a + " * " + b + " = " + (a * b));
     }
 
     private static void divide(int a, int b) {
-        
+        System.out.println(a + " / " + b + " = " + (a / b));
     }
 
     private static void modulo(int a, int b) {
